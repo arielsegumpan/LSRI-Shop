cd5368bb062470cd510b6582d163b7e3372fb690
asegumpan <ariel.segumpan@csav.edu.ph>
2025-03-13 15:48:36 +0800

    update models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'is_visible'
+    ];
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
 }
