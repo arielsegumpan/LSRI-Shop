9a92fce396eea0666a4451e03c03e7c995112ede
asegumpan <ariel.segumpan@csav.edu.ph>
2025-05-17 21:59:47 +0800

    update shop page

--- resources/views/livewire/pages/shop-page.blade.php
+++ resources/views/livewire/pages/shop-page.blade.php
@@ -121,34 +121,28 @@
                                 {{ __('Price') }}
                             </h3>
                             <div class="flex flex-col gap-y-4 mt-5">
-                                <div class="relative flex items-start flex-row">
-                                    <div class="flex items-center h-5">
-                                        <input type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-blue-600 focus:ring-blue-500 checked:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800 p-3" id="hs-default-checkbox">
-                                    </div>
-                                    <label for="hs-checkbox-delete" class="ms-3">
-                                        <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-300">{{ __('₱ 500 Below') }}</span>
-                                    </label>
-                                </div>
-                                <div class="relative flex items-start flex-row">
-                                    <div class="flex items-center h-5">
-                                        <input type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-blue-600 focus:ring-blue-500 checked:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800 p-3" id="hs-default-checkbox">
-                                    </div>
-                                    <label for="hs-checkbox-delete" class="ms-3">
-                                        <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-300">{{ __('₱ 500 - 1000') }}</span>
-                                    </label>
-                                </div>
-                                <div class="relative flex items-start flex-row">
-                                    <div class="mb-[0.125rem] block min-h-[1.5rem] ps-[1.5rem]">
+
+                                <div class="relative flex items-start space-x-3">
+                                    <div class="flex items-center h-6">
                                         <input
-                                        class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-red-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-red checked:bg-red checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-red checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-red checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-red dark:checked:bg-red"
                                         type="checkbox"
-                                        value=""
-                                        id="hs-default-checkbox" />
+                                        id="custom-red-checkbox"
+                                        class="w-4 h-4 border-2 border-gray-300 rounded-sm
+                                                bg-transparent
+                                                text-red-600 focus:ring-1 focus:ring-red-500 focus:border-red-500
+                                                checked:bg-red-600 checked:border-red-800
+                                                dark:bg-neutral-900 dark:border-neutral-800
+                                                dark:checked:bg-red-800 dark:checked:border-red-800
+                                                dark:focus:ring-red-800 transition-all duration-150 ease-in-out"
+                                        >
                                     </div>
-                                    <label for="hs-default-checkbox" class="ms-3">
-                                        <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-300">{{ __('₱ 1000 above') }}</span>
+                                    <label for="custom-red-checkbox" class="text-sm font-medium text-gray-900 dark:text-neutral-200">
+                                        {{ __('₱ 500 Below') }}
                                     </label>
                                 </div>
+
+
+
                             </div>
 
                         </div>
@@ -163,16 +157,32 @@
 
                             <div class="mb-[0.125rem] block min-h-[1.5rem] ps-[1.5rem]">
                                 <input
-                                    class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-red-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-red checked:bg-red checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-red dark:checked:bg-red"
+                                    class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-red-500 outline-none
+                                        before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-['']
+                                        checked:border-red checked:bg-red checked:before:opacity-[0.16]
+                                        checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-red-600 checked:after:bg-transparent checked:after:content-['']
+                                        hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60
+                                        focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s]
+                                        focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-['']
+                                        checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]
+                                        checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-red-600 checked:focus:after:bg-transparent
+                                        rtl:float-right
+                                        dark:border-neutral-400 dark:checked:border-red dark:checked:bg-red"
                                     type="checkbox"
                                     value=""
-                                    id="checkboxDefault" />
+                                    id="checkboxDefault"
+                                />
                                 <label
                                     class="inline-block ps-[0.15rem] hover:cursor-pointer"
                                     for="checkboxDefault">
                                     Default checkbox
                                 </label>
                             </div>
+
+
+
+
+
 
 
                         </div>
